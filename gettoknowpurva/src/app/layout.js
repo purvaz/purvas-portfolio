8936ca90899d260b760bcc3b737aed8fbcc90ee0
purvaz <pzinjarde@sfsu.edu>
2023-10-03 14:44:16 -0700

    analytics added

--- gettoknowpurva/src/app/layout.js
+++ gettoknowpurva/src/app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Navigation from './components/navbar'
+import { Analytics } from '@vercel/analytics/react';
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -17,6 +18,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Navigation />
         {children}
+        <Analytics />
       </body>
     </html>
   )
